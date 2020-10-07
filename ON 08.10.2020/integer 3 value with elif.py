8a3ecfb7ab6a37e88f07413510c43ca7791df54d
Vitaliy <dargven@yandex.ru>
2020-10-07 23:20:59 +0400

    added def to int 3 value

--- ON 08.10.2020/integer 3 value with elif.py
+++ ON 08.10.2020/integer 3 value with elif.py
@@ -1,9 +1,11 @@
 a = int(input('Введите тута первое число: '))
 b = int(input('Здеся второе: '))
 c = int(input('Кстати, а как решать задачу с шахматами? Ой, 3 число: '))
+def print_min(min):
+    print('Минимальное число = ', min)
 if a > b < c:
-    print(b)
+    print_min(b)
 elif b > a < c:
-    print(a)
+    print_min(a)
 else:
-    print(c)
+    print_min(c)
